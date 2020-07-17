91ddde300ca820c4ab75b26f13c5dd23e1756908
nakphero <67516989+nakphero@users.noreply.github.com>
2020-07-17 22:30:51 +0600

    working on assignment 3

---
+++ README.md
@@ -0,0 +1,1 @@
+# feet-wood-brick-tiny
---
+++ assignment.js
@@ -0,0 +1,16 @@
+// feetToMile
+function inchToFeet (inch){
+    var feet = inch /12;
+    return feet;
+}
+var result = inchToFeet(144);
+console.log(result);
+// woodCalculator
+function fruitCalculator (mango, apple){
+    var mangoCount = mango *4;
+    var appleCount = apple *4;
+    var totalFruit = mangoCount + appleCount;
+    return totalFruit;
+}
+// brickCalculator
+// tinyFriend
